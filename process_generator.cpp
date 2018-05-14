f4064bee54498aba187ed7e6b9700bb2ca8d8225
Eliot Diaz <eliot.daz@gmail.com>
2018-05-14 11:24:48 -0400

    Se completa la generacion de procesos colocando una ventana para la seleccion de la duracion de las rafagas (use_time de Resources) y la creacion de dichos recursos (instancias de la clase Resources).

--- process_generator.cpp
+++ process_generator.cpp
@@ -27,12 +27,13 @@ class Process {
 
 		Process() {};
 		
-		Process(int pid, int max_life_time, float nice_probability, float type_probability){
+		Process(int pid, int max_life_time, float nice_probability, float type_probability, float window_size){
 			unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 			default_random_engine generator(seed);
 			uniform_int_distribution<int> dist(1,10);		
 			int aux;
 			int u_t;
+			int upper_bound;
 
 			// En caso de que consideremos el Real-Time Scheduler. El proceso puede ser de 
 			// tipo CFS con una probabilidad type_probability o Real-Time con una probabilidad
@@ -63,24 +64,36 @@ class Process {
 
 			dist = uniform_int_distribution<int>(1,max_life_time);
 			life_time = dist(generator);
+			upper_bound = (int)ceil(life_time * window_size);
 			aux = 0;
 			while (aux < life_time) {
-				dist = uniform_int_distribution<int>(aux+1,life_time);
+				cout << "Low bound " << (aux+1) << endl;
+				cout << "Upper bound " << upper_bound << endl;
+				dist = uniform_int_distribution<int>(aux+1,upper_bound);
 				u_t = dist(generator);
-				aux += u_t;
-				//requirements.push_back(Resource(u_t,));
-				cout << " " << u_t << " ";
+				cout << "u_t " << u_t << endl;
+				requirements.push_back(Resource("CPU",u_t-aux));
+				upper_bound += u_t-aux;
+				aux = u_t;
+				if (upper_bound > life_time){
+					upper_bound = life_time;
+				}
 			}
 			cout << endl;
 			v_runtime = 0;
-			
+			cout << "Tipo: " << type << endl;
 			cout << "PID: " << PID << endl;
 			cout << "Nice value: " << nice << endl;
 			cout << "Life time value: " << life_time << endl;
+			cout << "Runtime: " << v_runtime << endl;
+			cout << "Requirements: " << endl;
+			for(int i = 0; i < requirements.size(); i++) {
+				cout << "    " << requirements[i].type << " " << requirements[i].use_time << endl;
+			}
 		}
 };
 
 int main(){
-	Process p(1,150,0.8,0.8);
+	Process p(1,1000,0.8,0.8,0.3);
 	return 0;
 }
